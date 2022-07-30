924c69c940b289928b4be7a2f4e7c72cb60d5aff
Yusuke Wada <yusuke@kamawada.com>
2022-07-30 21:39:36 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Handler } from 'hono'
+
+export const hello = (message: string = 'Hello'): Handler => {
+  return async (c, next) => {
+    await next()
+    c.res.headers.append('X-Message', message)
+  }
+}
