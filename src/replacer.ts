c97a5a69f426e13c1ffb35291de883061fe97e66
Yusuke Wada <yusuke@kamawada.com>
2022-08-02 08:26:39 +0900

    update replacer for versioning modules

--- src/replacer.ts
+++ src/replacer.ts
@@ -1,24 +1,24 @@
 // @denoify-ignore
 import { makeThisModuleAnExecutableReplacer } from 'denoify'
 
-makeThisModuleAnExecutableReplacer(async ({ parsedImportExportStatement }) => {
+makeThisModuleAnExecutableReplacer(async ({ parsedImportExportStatement, version }) => {
   if (parsedImportExportStatement.parsedArgument.nodeModuleName === 'hono') {
     if ((parsedImportExportStatement as any)['statementType'] === 'declare module') {
-      return 'declare module "https://deno.land/x/hono@v2.0.6/mod.ts"'
+      return `declare module "https://deno.land/x/hono@v${version}/mod.ts"`
     } else {
-      return 'import type { Handler, Context } from "https://deno.land/x/hono@v2.0.6/mod.ts"'
+      return `import type { Handler, Context } from "https://deno.land/x/hono@v${version}/mod.ts"`
     }
   }
 
   if (parsedImportExportStatement.parsedArgument.nodeModuleName === 'validator') {
     if (!parsedImportExportStatement.isAsyncImport) {
-      return 'import validator from "https://esm.sh/validator@13.7.0"'
+      return `import validator from "https://esm.sh/validator@${version}"`
     } else {
-      return 'import ("https://esm.sh/validator@13.7.0")'
+      return `import ("https://esm.sh/validator@${version}")`
     }
   }
   if (parsedImportExportStatement.parsedArgument.nodeModuleName === 'jsonpath-plus') {
-    return 'import { JSONPath } from "https://esm.sh/jsonpath-plus@7.0.0"'
+    return `import { JSONPath } from "https://esm.sh/jsonpath-plus@${version}"`
   }
   return undefined
 })
